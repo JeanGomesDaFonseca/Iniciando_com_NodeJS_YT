304cc02fc06722b9e0bf149decd769df57e3d37d
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2023-07-19 20:09:20 -0300

    começando usar o express

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { Person } = require("./person");
 
 // require("./modules/path");
 // require("./modules/fs");
-require("./modules/http");
-
+// require("./modules/http");
+require("./modules/express");
 
 const person = new Person("Jean Gomes");
 
